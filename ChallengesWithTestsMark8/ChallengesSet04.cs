9a6995b6eeb8303c70255e8c02cd3c87b20b8ec4
Brooks Stockley <bstockley26@gmail.com>
2024-06-19 13:34:57 -0400

    finished set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -6,47 +6,192 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int result = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    result += num; 
+                }
+                else
+                {
+                    result -= num; 
+                }
+            }
+
+            return result;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2) ||
+        string.IsNullOrEmpty(str3) || string.IsNullOrEmpty(str4))
+            {
+                return 0;
+            }
+
+            int shortestLength = int.MaxValue; 
+
+            
+            if (str1.Length < shortestLength)
+            {
+                shortestLength = str1.Length;
+            }
+            if (str2.Length < shortestLength)
+            {
+                shortestLength = str2.Length;
+            }
+            if (str3.Length < shortestLength)
+            {
+                shortestLength = str3.Length;
+            }
+            if (str4.Length < shortestLength)
+            {
+                shortestLength = str4.Length;
+            }
+
+            return shortestLength;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int smallestNumber = number1; 
+
+           
+            if (number2 < smallestNumber)
+            {
+                smallestNumber = number2;
+            }
+
+            
+            if (number3 < smallestNumber)
+            {
+                smallestNumber = number3;
+            }
+
+            
+            if (number4 < smallestNumber)
+            {
+                smallestNumber = number4;
+            }
+
+            return smallestNumber;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            if (biz == null)
+            {
+                throw new ArgumentNullException(nameof(biz), "Business object cannot be null.");
+            }
+
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 <= 0 || sideLength2 <= 0 || sideLength3 <= 0)
+            {
+                return false;
+            }
+
+            
+            bool condition1 = (sideLength1 + sideLength2) > sideLength3;
+            bool condition2 = (sideLength1 + sideLength3) > sideLength2;
+            bool condition3 = (sideLength2 + sideLength3) > sideLength1;
+
+            return condition1 && condition2 && condition3;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false; 
+            }
+
+            
+            if (double.TryParse(input, out double result))
+            {
+                return true; 
+            }
+
+            return false;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            if (objs == null || objs.Length == 0)
+            {
+                return false; 
+            }
+
+            int countNull = 0;
+            int countNonNull = 0;
+
+            foreach (object obj in objs)
+            {
+                if (obj == null)
+                {
+                    countNull++;
+                }
+                else
+                {
+                    countNonNull++;
+                }
+            }
+
+            return countNull > countNonNull;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            int countEvens = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0) // Check if the number is even
+                {
+                    sum += num;
+                    countEvens++;
+                }
+            }
+
+            if (countEvens == 0)
+            {
+                return 0;
+            }
+
+            return (double)sum / countEvens;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
         }
     }
 }
